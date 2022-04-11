bea0e658bb394e13924353bc43248754ddf38c80
Selin Su Varol <100427748+selinsuvarol@users.noreply.github.com>
2022-04-12 00:17:40 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const num1 = 2;
 const num2 = 31;
 const multiply = num1 * num2;
-const random = Math.floor( Math.random() * 10 ) + 1;
+const random = Math.floor( Math.random() * 10000 ) + 1;
 const num3 = 16;
 const num4 = 6;
 const mod = num3 % num4;
